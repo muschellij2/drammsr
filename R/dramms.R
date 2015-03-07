e95744ba4c73e336cb9ae219bbb136b245ca3d23
muschellij2 <muschellij2@gmail.com>
2015-03-07 17:06:39 -0500

    Trying to github

---
+++ R/dramms.R
@@ -0,0 +1,25 @@
+
+#' @title Run DRAMMS
+#'
+#' @description Runs DRAMMS on source and target
+#' @param source Filename (or nifti) to match to target
+#' @param target Filename (or nifti) to match source to to target
+#' @param outimg Output filename
+#' @param outdef output deformation field
+#' @param retimg return nifti object versus output image
+#' @export
+#' @keywords
+#' @seealso
+#' @return If \code{retimg = TRUE}, a \code{nifti} object.  
+#' If \code{retimg = FALSE}, a the filename of the output image.
+dramms <- function(
+  source, # Filename (or nifti) to match to target
+  target, # Filename (or nifti) to match source to to target
+  outimg = NULL, # Output filename
+  outdef = NULL, # output deformation field
+  retimg = FALSE # return nifti object versus output image
+  ){
+#   dramms --source sourceimage.hdr --target targetimage.nii
+#   --outimg outimage.img --outdef outdef.nii.gz  
+  
+}
---
+++ R/drammsr.R
@@ -0,0 +1,7 @@
+#' @name drammsr-package
+#' @aliases drammsr-package
+#' @aliases drammsr
+#' @docType package
+#' @title Deformable Registration via Attribute Matching and Mutual-Saliency Weighting
+#' @description This package performs image registration using attribute weighting
+#' @author John Muschelli
