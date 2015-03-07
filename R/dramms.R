a768fca16b33a943cba64d4f96b62fd802da6996
muschellij2 <muschellij2@gmail.com>
2015-03-07 18:54:30 -0500

    fixed warnings as errors, now false

--- R/dramms.R
+++ R/dramms.R
@@ -12,6 +12,14 @@
 #' @export
 #' @return If \code{retimg = TRUE}, a \code{nifti} object.  
 #' If \code{retimg = FALSE}, a the filename of the output image.
+#' @examples \dontrun{
+#' require(fslr)
+#' temp = file.path(fsldir(), "data", "standard", "MNI152_T1_1mm.nii.gz")
+#' # get file
+#' #  https://github.com/muschellij2/FSLR_Data/raw/master/01-Baseline_T1.nii.gz
+#' brain = file.path(fsldir(), "data", "standard", "MNI152_T1_1mm_brain.nii.gz")
+#' res = dramms(source = brain, target = temp, retimg = TRUE)
+#' }
 dramms <- function(
   source, # Filename (or nifti) to match to target
   target, # Filename (or nifti) to match source to to target
