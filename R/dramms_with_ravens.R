58868a2a510c08d6330507469f1661746370e591
muschellij2 <muschellij2@gmail.com>
2016-02-16 16:22:02 -0500

    fixing the default for dramms_with_ravens

--- R/dramms_with_ravens.R
+++ R/dramms_with_ravens.R
@@ -5,7 +5,7 @@
 #' @param source Filename (or nifti) to match to target
 #' @param target Filename (or nifti) to match source to to target
 #' @param outfile Output filename
-#' @param outdef output deformation field
+#' @param outdef output deformation field (it will be in template to subject space)
 #' @param retimg return nifti object versus output image
 #' @param opts Extra arguments to pass to \code{dramms}
 #' @param label label image in subject space. 
@@ -18,8 +18,10 @@
 #' @import fslr 
 #' @import oro.nifti
 #' @export
-#' @return If \code{retimg = TRUE}, a \code{nifti} object.  
-#' If \code{retimg = FALSE}, a the filename of the output image.
+#' @return If \code{retimg = TRUE}, a list of \code{nifti} objects consisting
+#' of the RAVENS images, the output file (template to subject space), and 
+#' output deformation field (template to subject space).
+#' If \code{retimg = FALSE}, a list of the filenames of the output image.
 #' @examples \dontrun{
 #' require(fslr)
 #' temp = file.path(fsldir(), "data", "standard", "MNI152_T1_1mm.nii.gz")
@@ -35,42 +37,62 @@ dramms_with_ravens <- function(
   outdef = NULL, # output deformation field
   retimg = FALSE, # return nifti object versus output image
   opts = NULL,
-  label = NULL, # Filename (or nifti) to apply deformation field
+  label, # Filename (or nifti) to apply deformation field
   labs = NULL,  
   ravens_prefix = NULL,
   verbose = TRUE
 ){
   source = checkimg(source)
   target = checkimg(target)
-  outfile = check_outfile(outfile = outfile, 
-                          retimg = retimg, 
-                          fileext = ".nii.gz")
   
-  lab_args = NULL
-  if (!is.null(label)){
-    if (is.null(ravens_prefix)) {
-      stop("RAVENS prefix must be specified!")
-    }
-    nlabel = check_nifti(label)
-    if ( is.null(labs) ) {
-      labs = unique(c(nlabel))
-      labs = labs[ labs > 0 ]
-      labs = sort(labs)
-    }
-    if (length(labs) == 0) {
-      stop("No positive labels in the image")
-    }
-    if (length(labs) > 5) {
-      stop("More than 5 labels in the image, specify labs")
-    }
-    labs = paste(labs, collapse = ",")
-    label = checkimg(label)
-    lab_args = c(
-      "-L" = label,
-      "-R" = ravens_prefix,
-      "-l" = labs)
+  stopifnot(!missing(label))
+  
+  if ( is.null(outdef) ) {
+    outdef = tempfile(fileext = ".nii.gz")
   }  
+  if ( is.null(outfile) ) {
+    outfile = tempfile(fileext = ".nii.gz")
+  }
   
+  ###########################
+  # Create the prefix if not specified
+  ###########################
+  lab_args = NULL
+  if (is.null(ravens_prefix)) {
+    ravens_prefix = tempfile()
+    # stop("RAVENS prefix must be specified!")
+  }
+  stopifnot(length(ravens_prefix) == 1)
+  
+  ###########################
+  # Create label vector if not specified
+  ###########################
+  if ( is.null(labs) ) {
+    nlabel = check_nifti(label)
+    labs = unique(c(nlabel))
+    labs = labs[ labs > 0 ]
+  } 
+  if (length(labs) == 0) {
+    stop("No positive labels in the image")
+  }
+  if (length(labs) > 5) {
+    stop("More than 5 labels in the image, must specify labs")
+  }
+  
+  ##############################################
+  # Put it in the syntax necessary for DRAMMS
+  ##############################################
+  labs = sort(labs)
+  xlabs = paste(labs, collapse = ",")
+  label = checkimg(label)
+  lab_args = c(
+    "-L" = label,
+    "-R" = ravens_prefix,
+    "-l" = xlabs)
+
+  ##############################################
+  # Create Args for labels
+  ##############################################  
   args = c("--source" = source, 
            "--target" = target,
            "--outimg" = outfile,
@@ -78,21 +100,28 @@ dramms_with_ravens <- function(
            lab_args,
            opts)
   
+  ##############################################
+  # Make DRAMMS Command
+  ##############################################    
   cmd = "dramms"
   cmd = dramms_cmd_maker(cmd = cmd, args = args)
   
   if (verbose) {
     cat(cmd, "\n")
-  }  
+  }
   res = system(cmd)
   
   if (res != 0) {
     stop("DRAMMS command failed")
   }
-  
+  ravens = paste0(ravens_prefix, labs, ".nii.gz")
   if (retimg) {
-    img = readNIfTI(outfile, reorient = FALSE)
-    return(img)
+    ravens = check_nifti(ravens)
+    outfile = check_nifti(outfile)
+    outdef = check_nifti(outdef)
   }
-  return(outfile)
+  L = list(ravens = ravens, 
+           outfile = outfile,
+           outdef = outdef)
+  return(L)
 }
